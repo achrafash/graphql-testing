0781651db2f19bac72f774153c88572c42ee9ab7
Achraf Ait Sidi Hammou <53062630+AchrafAsh@users.noreply.github.com>
2022-03-24 12:28:45 +0100

    refactor: use @graphql-tools/schema instead

--- src/typeDefs.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs')
-const path = require('path')
-const { buildSchema } = require('graphql')
-
-module.exports = buildSchema(
-    fs.readFileSync(path.join(__dirname, 'schema.gql'))
-)
